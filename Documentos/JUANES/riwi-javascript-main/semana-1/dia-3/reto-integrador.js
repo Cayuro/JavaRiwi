62ed0935412b36d196ae994e2f9ed45ef5b9984f
ubuntu <example@gmail.com>
2026-01-06 14:01:50 -0500

    closure

--- Documentos/JUANES/riwi-javascript-main/semana-1/dia-3/reto-integrador.js
+++ Documentos/JUANES/riwi-javascript-main/semana-1/dia-3/reto-integrador.js
@@ -1,84 +1,102 @@
-/*
-Crea un programa que evalúe una nota y muestre si aprueba o reprueba,
-usando funciones, condicionales y ciclos.
-*/
+// /*
+// Crea un programa que evalúe una nota y muestre si aprueba o reprueba,
+// usando funciones, condicionales y ciclos.
+// */
 
 
 
-function matAprovation(nombre,nota){
-    let aprove = false;
-    if(nota >= 3){
-        aprove = true;
-    }
-    console.log(`${nombre} aprove the course: ${aprove}`)
-}
+// function matAprovation(nombre,nota){
+//     let aprove = false;
+//     if(nota >= 3){
+//         aprove = true;
+//     }
+//     console.log(`${nombre} aprove the course: ${aprove}`)
+// }
 
-matAprovation("Juanes",4)
+// matAprovation("Juanes",4)
 
-let  counter= 0;
-console.log(counter);  //  ->  undefined
-{
-         let counter  =  1;
-         console.log(counter);
-         counter += 2; //  ->  1
-}
-counter  =  counter  +  1;
-console.log(counter);  //  ->  2
-{
-    let counter;
-    while(counter <10){
-        counter = counter + 1
-    }
-}
-console.log(counter)
+// let  counter= 0;
+// console.log(counter);  //  ->  undefined
+// {
+//          let counter  =  1;
+//          console.log(counter);
+//          counter += 2; //  ->  1
+// }
+// counter  =  counter  +  1;
+// console.log(counter);  //  ->  2
+// {
+//     let counter;
+//     while(counter <10){
+//         counter = counter + 1
+//     }
+// }
+// console.log(counter)
+
+// // const contenedor = document.querySelector("body");
+// // const h1DOM = document.createElement("H1");
+
+// // let contador = 10;
+
+// // h1DOM.textContent= contador;
+// // contenedor.appendChild(h1DOM);
+
+// // for (let i = 10; i > 0; i--) {
+
+// //     setTimeout(() => {
+// //         contador--;
+// //         h1DOM.textContent = contador;
+// //         if (contador==0) {
+// //         contenedor.style.backgroundColor= "red";
+// //         h1DOM.style.color= "white";
+// //     }
+// //     }, i*500);
+    
+// // }
 
 // const contenedor = document.querySelector("body");
 // const h1DOM = document.createElement("H1");
-
-// let contador = 10;
-
-// h1DOM.textContent= contador;
 // contenedor.appendChild(h1DOM);
 
-// for (let i = 10; i > 0; i--) {
+// const esperar = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
-//     setTimeout(() => {
-//         contador--;
-//         h1DOM.textContent = contador;
-//         if (contador==0) {
-//         contenedor.style.backgroundColor= "red";
-//         h1DOM.style.color= "white";
-//     }
-//     }, i*500);
+// async function iniciarCuentaRegresiva() {
+//     let contador = 10;
     
+//     // Usamos un bucle que decrementa hasta 0
+//     for (let i = contador; i >= 0; i--) {
+//         // 1. Actualizamos el texto primero
+//         h1DOM.textContent = i;
+
+//         // 2. Si llegamos a 0, cambiamos el color y terminamos
+//         if (i === 0) {
+//             contenedor.style.backgroundColor = "red";
+//             h1DOM.style.color= "white";
+//             h1DOM.style.justifySelf= "center";
+//             h1DOM.style.margin= "200px";
+//             break; // Salimos del bucle
+//         }
+
+//         // 3. ESPERAMOS un segundo antes de volver a empezar el bucle
+//         await esperar(500);
+//     }
 // }
 
-const contenedor = document.querySelector("body");
-const h1DOM = document.createElement("H1");
-contenedor.appendChild(h1DOM);
+// iniciarCuentaRegresiva();
 
-const esperar = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-
-async function iniciarCuentaRegresiva() {
-    let contador = 10;
-    
-    // Usamos un bucle que decrementa hasta 0
-    for (let i = contador; i >= 0; i--) {
-        // 1. Actualizamos el texto primero
-        h1DOM.textContent = i;
-
-        // 2. Si llegamos a 0, cambiamos el color y terminamos
-        if (i === 0) {
-            contenedor.style.backgroundColor = "red";
-            h1DOM.style.color= "white";
-            h1DOM.style.justifySelf= "center";
-            h1DOM.style.margin= "200px";
-            break; // Salimos del bucle
-        }
-
-        // 3. ESPERAMOS un segundo antes de volver a empezar el bucle
-        await esperar(500);
-    }
+function crearSaludador(saludo) {
+  // Esta variable 'saludo' vive en el scope externo
+  return function(nombre) {
+    console.log(saludo + ", " + nombre);
+  };
 }
 
-iniciarCuentaRegresiva();
+const saludarHola = crearSaludador("Hola");
+const saludarChao = crearSaludador("Adiós");
+const cualquiera = crearSaludador("Madre de dios purisima");
+
+saludarHola("Elena"); // "Hola, Elena"
+saludarChao("Carlos"); // "Adiós, Carlos"
+cualquiera("Juanes");
+saludarHola("camilo");
+saludarChao("Andrea");
+cualquiera("Es un día muy feliz")
