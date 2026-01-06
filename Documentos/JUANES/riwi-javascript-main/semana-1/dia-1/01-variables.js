a4c840e7cdf114459a765c7a58efbaa67f35f8aa
ubuntu <example@gmail.com>
2026-01-06 07:29:03 -0500

    primero

--- Documentos/JUANES/riwi-javascript-main/semana-1/dia-1/01-variables.js
+++ Documentos/JUANES/riwi-javascript-main/semana-1/dia-1/01-variables.js
@@ -2,3 +2,13 @@
 Declara variables nombre y edad.
 Muestra un mensaje en consola usando esas variables.
 */
+//STRING
+let name = "Juanes";
+let age = 25;
+const residence = "Colombiano";
+const initialMessage = "Welcome";
+
+// NUMEROS
+let int = 50;
+let dec = 10.33;
+let negative = -22;
