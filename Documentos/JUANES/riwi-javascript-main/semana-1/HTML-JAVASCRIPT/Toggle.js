e42fc4429bb21039e62147cae6a9e9c78f241dad
Cayuro <Juan_gomez2000@hotmail.com>
2026-01-07 17:54:12 -0500

    toggle

--- Documentos/JUANES/riwi-javascript-main/semana-1/HTML-JAVASCRIPT/Toggle.js
+++ Documentos/JUANES/riwi-javascript-main/semana-1/HTML-JAVASCRIPT/Toggle.js
@@ -3,19 +3,26 @@ const tarjeta = document.getElementById("tarjeta");
 const tarjeta2 = document.getElementById("tarjeta2");
 const h3 = document.getElementById("h3");
 
+
 toggle.addEventListener('click', () => {
     tarjeta.classList.toggle('dark');
-    if (h3.style.backgroundColor == "black"){
+    if (tarjeta2.style.backgroundColor == "rgb(26, 38, 50)"){
         console.log("hola");
         tarjeta2.style.backgroundColor = "white"
-        h3.style.backgroundColor = "white";
+        // h3.style.backgroundColor = "white";
         h3.style.color = "black";
-        console.log(h3.style.backgroundColor == "black")
+        console.log(h3.style.backgroundColor == "gray")
     }
     else {
-        h3.style.backgroundColor = "black";
+        // h3.style.backgroundColor = "rgb(26, 38, 50)";
         h3.style.color = "white";
-        tarjeta2.style.backgroundColor = "black"
+        tarjeta2.style.backgroundColor = "rgb(26, 38, 50)"
     }
 })
 
+// TOGGLE PAGE THEME - (DARK/WHITE)
+const togglePage = document.getElementById("togglePage");
+
+togglePage.addEventListener("click", ()=>{
+    document.body.classList.toggle('dark')
+})
