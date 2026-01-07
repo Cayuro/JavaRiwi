81bedb283c0f8e7570ad330705a14f3ffee27460
Cayuro <Juan_gomez2000@hotmail.com>
2026-01-07 18:10:17 -0500

    ageValidation

--- Documentos/JUANES/riwi-javascript-main/semana-1/HTML-JAVASCRIPT/ageValidation.js
+++ Documentos/JUANES/riwi-javascript-main/semana-1/HTML-JAVASCRIPT/ageValidation.js
@@ -1,16 +1,22 @@
 const inputAge = document.getElementById("inputAge");
 const buttonEligibility = document.getElementById("buttonEligibility");
+const divEligibility = document.getElementById("divElegibility");
 
 buttonEligibility.addEventListener('click', ()=> {
+    
     if ((inputAge.value >= 18) && (inputAge.value < 80)){
         alert("¡Congrats! You are elegibility")
+        divEligibility.classList.remove("hidden")
     }
     else if ((inputAge.value < 18) && (inputAge.value >= 0)){
         alert("Sorry, you are minor, is not posibble to select you");
+        divEligibility.classList.add("hidden")
     }
     else {
         alert("Prompt a real age, or a age less than 80")
+        divEligibility.classList.add("hidden")
     }
+    inputAge.value = "";
 });
 
 inputAge.addEventListener('input', ()=>{
