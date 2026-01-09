21c97a508dd9d89857a646bc9e500350eaee1b17
Cayuro <juan_gomez2000@hotmail.com>
2026-01-09 11:50:03 -0500

    commit

---
+++ Documentos/JUANES/riwi-javascript-main/taskDelQuesabe.js
@@ -0,0 +1,37 @@
+// const inputTaskManager = document.getElementById("taskInput");
+// const btnTask = document.getElementById("btnAddTask");
+// const divTask = document.getElementById("divTask");
+
+
+// const templateTask = divTask.firstElementChild;
+
+// //----------------------------------------------------
+
+// function addNewTask(text){
+//     /** @type {HTMLDivElement} */
+//     const newTask = templateTask.cloneNode(true)
+
+//     const btnDelete = newTask.querySelector("button");
+
+
+//     btnDelete.addEventListener("click",()=>{
+//         newTask.remove()
+//     })
+    
+//     const textoTask = newTask.querySelector(".text-sm ")
+//     textoTask.textContent = text
+
+
+//     divTask.appendChild(newTask)
+// }
+
+//  //-------------------------------
+
+//  btnTask.addEventListener("click", ()=>{
+//     if (inputTaskManager.value.trim() == ""){
+//         inputTaskManager.value = ""
+//         return}
+//     addNewTask(inputTaskManager.value)
+//     inputTaskManager.value = ""
+//  })
+
