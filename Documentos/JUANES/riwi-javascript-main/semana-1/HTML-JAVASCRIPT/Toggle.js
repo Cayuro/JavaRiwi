1dbbf9ca7c848a10029d023db93e6500b84b2538
Cayuro <Juan_gomez2000@hotmail.com>
2026-01-08 21:25:35 -0500

    commit taskManager

--- Documentos/JUANES/riwi-javascript-main/semana-1/HTML-JAVASCRIPT/Toggle.js
+++ Documentos/JUANES/riwi-javascript-main/semana-1/HTML-JAVASCRIPT/Toggle.js
@@ -2,21 +2,21 @@ const toggle = document.getElementById("buttonToggle");
 const tarjeta = document.getElementById("tarjeta");
 const tarjeta2 = document.getElementById("tarjeta2");
 const h3 = document.getElementById("h3");
-
+const pageTitle = document.getElementById("pageTitle");
 
 toggle.addEventListener('click', () => {
-    tarjeta.classList.toggle('dark');
+    // tarjeta.classList.toggle('dark');
     if (tarjeta2.style.backgroundColor == "rgb(26, 38, 50)"){
-        console.log("hola");
-        tarjeta2.style.backgroundColor = "white"
+        tarjeta2.style.backgroundColor = "white";
         // h3.style.backgroundColor = "white";
         h3.style.color = "black";
-        console.log(h3.style.backgroundColor == "gray")
+         tarjeta.classList.toggle('dark');
     }
     else {
         // h3.style.backgroundColor = "rgb(26, 38, 50)";
         h3.style.color = "white";
-        tarjeta2.style.backgroundColor = "rgb(26, 38, 50)"
+        tarjeta2.style.backgroundColor = "rgb(26, 38, 50)";
+         tarjeta.classList.toggle('dark');
     }
 })
 
@@ -24,5 +24,6 @@ toggle.addEventListener('click', () => {
 const togglePage = document.getElementById("togglePage");
 
 togglePage.addEventListener("click", ()=>{
-    document.body.classList.toggle('dark')
+    document.body.classList.toggle('dark');
+    pageTitle.style.color= "black";
 })
---
+++ Documentos/JUANES/riwi-javascript-main/semana-1/HTML-JAVASCRIPT/taskManager.js
@@ -0,0 +1,7 @@
+const taskInput = document.getElementById("taskInput");
+const addTask = document.getElementById("addInput");
+const taskBurned = document.querySelector("span.dark\\:text-gray-200");
+
+console.log(taskBurned);
+console.log(addTask);
+console.log(taskInput)
