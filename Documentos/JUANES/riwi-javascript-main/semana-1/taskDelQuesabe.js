eff0a759b9ed9b534a8d2bf9f98a0fdd056c92c8
Cayuro <juan_gomez2000@hotmail.com>
2026-01-13 13:57:32 -0500

    hola_practicando

--- Documentos/JUANES/riwi-javascript-main/semana-1/taskDelQuesabe.js
+++ Documentos/JUANES/riwi-javascript-main/semana-1/taskDelQuesabe.js
@@ -35,3 +35,23 @@
 //     inputTaskManager.value = ""
 //  })
 
+const personas = {
+    nombre: "calvito",
+    edad: 12,
+    info: {
+        valor: 12000,
+        tipo: "persona",
+        locura:{
+            quieto: "brother"
+        }
+    }
+}
+
+// const nombre = personas.nombre;
+// const valor = personas.info.valor;
+// const quieto = personas.info.locura.quieto;
+
+let {nombre, info:{valor, locura:{quieto}}} = personas
+nombre = "Care loco inmundo"
+// valor = negativo
+console.log(nombre, valor, quieto)
