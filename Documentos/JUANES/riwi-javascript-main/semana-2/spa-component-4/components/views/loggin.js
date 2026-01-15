e00dd1ec20408f25bee7f3c32976bf34401408a5
Cayuro <juan_gomez2000@hotmail.com>
2026-01-15 14:00:51 -0500

    cambios

--- Documentos/JUANES/riwi-javascript-main/semana-2/spa-component-4/components/views/loggin.js
+++ Documentos/JUANES/riwi-javascript-main/semana-2/spa-component-4/components/views/loggin.js
@@ -1,10 +1,27 @@
 export function loggin(){
-    return `<label for="nombre">Nombre:</label>
+    return `<form id="formulario"><label for="nombre">Nombre:</label>
             <input type="text" id="nombre" name="nombre" required><br><br>
 
             <label for="contraseña">Contraseña:</label>
             <input type="password" id="contraseña" name="contraseña" required><br><br>
 
             <button type="submit">Enviar</button>
-            `
+            </form>`
+}
+
+export function formm() {
+    const form = document.getElementById('formulario')
+    
+form.addEventListener('click', ()=> {
+    if(!formm){return};
+
+    const nombre = document.getElementById('nombre');
+    const contraseña = document.getElementById('contraseña')
+    if (nombre == 'juanes' && contraseña == '1234') {
+        location.hash = '#/home'
+    } else {
+        alert('ingrese bien bobo hpta')
+    }
+})
+
 }
